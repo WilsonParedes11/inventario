d0968a289ff533bd02b1d12fce6a8173f2da0c29
WILSON PAREDES <wilsonparedes87@gmail.com>
2024-08-01 22:43:21 -0500

    Actualizacion de la vista del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,77 +1,67 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="card card-md">
-    <div class="card-body">
-        <h2 class="h2 text-center mb-4">
+  <div class="d-flex justify-content-center align-items-center vh-100 ">
+    <div class="d-flex align-items-center bg-white p-4 rounded bg-danger">
+      <div style="margin-right: 20px;">
+        <a class="navbar-brand navbar-brand-autodark">
+          <img src="{{ asset('static/logo.png') }}" width="110" height="84"
+            alt="Tabler" class="navbar-brand-image" style="height: 80%">
+        </a>
+      </div>
+      <div class="card-md" style="width: 600px;">
+        <div class="card-body">
+          <h2 class="h2 text-center mb-4">
             Ingrese a su cuenta
-        </h2>
-        <form action="{{ route('login') }}" method="POST" autocomplete="off">
+          </h2>
+          <form action="{{ route('login') }}" method="POST" autocomplete="off">
             @csrf
             <div class="mb-3">
-                <label for="email" class="form-label">
-                    Dirección de correo electrónico
-                </label>
-                <input type="email"
-                       name="email"
-                       id="email"
-                       class="form-control @error('email') is-invalid @enderror"
-                       placeholder="tu@correo electrónico.com"
-                       autocomplete="off"
-                       value="{{ old('email') }}"
-                >
+              <label for="email" class="form-label">
+                Dirección de correo electrónico
+              </label>
+              <input type="email" name="email" id="email"
+                class="form-control @error('email') is-invalid @enderror"
+                placeholder="tu@correo electrónico.com" autocomplete="off"
+                value="{{ old('email') }}">
 
-                @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-2">
-                <label for="password" class="form-label">
-                    Contraseña
-                </label>
-
-                <div class="input-group input-group-flat">
-                    <input type="password"
-                           name="password"
-                           id="password"
-                           class="form-control @error('password') is-invalid @enderror"
-                           placeholder="Tu contraseña"
-                           autocomplete="off"
-                    >
-
-                    @error('password')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
+              @error('email')
+                <div class="invalid-feedback">
+                  {{ $message }}
                 </div>
+              @enderror
             </div>
-
             <div class="mb-2">
-                <label for="remember" class="form-check">
-                    <input type="checkbox" id="remember" name="remember" class="form-check-input"/>
-                    <span class="form-check-label">Recuérdame en este dispositivo</span>
-                </label>
+              <label for="password" class="form-label">
+                Contraseña
+              </label>
+              <div class="input-group input-group-flat">
+                <input type="password" name="password" id="password"
+                  class="form-control @error('password') is-invalid @enderror"
+                  placeholder="Tu contraseña" autocomplete="off">
+                @error('password')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
+              </div>
             </div>
-
+            <div class="mb-2">
+              <label for="remember" class="form-check">
+                <input type="checkbox" id="remember" name="remember"
+                  class="form-check-input" />
+                <span class="form-check-label">Recuérdame en este
+                  dispositivo</span>
+              </label>
+            </div>
             <div class="form-footer">
-                <button type="submit" class="btn btn-primary w-100">
-                    Iniciar sesión
-                </button>
+              <button type="submit" class="btn btn-primary w-100">
+                Iniciar sesión
+              </button>
             </div>
-        </form>
+          </form>
+        </div>
+      </div>
     </div>
-</div>
-<div class="text-center text-secondary mt-3">
-    {{-- ¿Aún no tienes cuenta?<a href="{{ route('register') }}" tabindex="-1">
-        Registrase
-    </a> --}}
-
-    {{-- <span class="form-label-description">
-        <a href="{{ route('password.request') }}">olvidé la contraseña</a>
-    </span> --}}
-</div>
+  </div>
 @endsection
--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -49,8 +49,8 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href="{{ url('/') }}">
-                        <img src="{{ asset('static/logo.png') }}" width="110" height="64" alt="Tabler"
-                            class="navbar-brand-image">
+                        <img src="{{ asset('static/logo.png') }}" width="110" alt="Tabler"
+                            class="navbar-brand-image" style="height:48px">
                     </a>
                 </h1>
                 <div class="navbar-nav flex-row order-md-last">
